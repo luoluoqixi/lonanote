85a6589978e0f3ddf1c5f38a11388dd5f1b5fd7b
luoluoqixi <luoqixi@luoqixi.com>
2025-02-14 23:44:02 +0800

    🚧 react-complex-tree

--- ui/src/renderer/src/pages/desktop/components/tabs/Explorer.tsx
+++ ui/src/renderer/src/pages/desktop/components/tabs/Explorer.tsx
@@ -1,10 +1,93 @@
 import React from 'react';
+import {
+  StaticTreeDataProvider,
+  Tree,
+  TreeItem,
+  TreeItemIndex,
+  UncontrolledTreeEnvironment,
+} from 'react-complex-tree';
+import 'react-complex-tree/lib/style-modern.css';
 
-import { Workspace, dialog, workspace, workspaceManager } from '@/bindings/api';
-import { Button, Heading, toaster } from '@/components/ui';
+import { Workspace } from '@/bindings/api';
+import { Button, Heading } from '@/components/ui';
 import { workspaceController, workspaceManagerController } from '@/controller/workspace';
 
 import styles from './Explorer.module.scss';
+
+const readTemplate = (
+  template: Record<string, any>,
+  data: { items: Record<TreeItemIndex, TreeItem<any>> } = { items: {} },
+) => {
+  for (const [key, value] of Object.entries(template)) {
+    data.items[key] = {
+      index: key,
+      canMove: true,
+      isFolder: value !== null,
+      children: value !== null ? Object.keys(value as object) : undefined,
+      data: key,
+      canRename: true,
+    };
+
+    if (value !== null) {
+      readTemplate(value, data);
+    }
+  }
+  return data;
+};
+
+const longTreeTemplate = {
+  root: {
+    Fruit: {
+      Apple: null,
+      Orange: null,
+      Lemon: null,
+      Berries: {
+        Strawberry: null,
+        Blueberry: null,
+      },
+      Banana: null,
+    },
+    Meals: {
+      America: {
+        SmashBurger: null,
+        Chowder: null,
+        Ravioli: null,
+        MacAndCheese: null,
+        Brownies: null,
+      },
+      Europe: {
+        Risotto: null,
+        Spaghetti: null,
+        Pizza: null,
+        Weisswurst: null,
+        Spargel: null,
+      },
+      Asia: {
+        Curry: null,
+        PadThai: null,
+        Jiaozi: null,
+        Sushi: null,
+      },
+      Australia: {
+        PotatoWedges: null,
+        PokeBowl: null,
+        LemonCurd: null,
+        KumaraFries: null,
+      },
+    },
+    Desserts: {
+      Cookies: null,
+      IceCream: null,
+    },
+    Drinks: {
+      PinaColada: null,
+      Cola: null,
+      Juice: null,
+    },
+  },
+};
+
+export const longTree = readTemplate(longTreeTemplate);
 
 const onOpenWorkspace = async () => {
   await workspaceManagerController.selectOpenWorkspace();
@@ -31,7 +114,20 @@ interface WorkspaceExplorerProps {
 }
 
 const WorkspaceExploreer = ({ workspace }: WorkspaceExplorerProps) => {
-  return <div>{workspace.metadata.name}</div>;
+  return (
+    <div>
+      {workspace.metadata.name}
+      <UncontrolledTreeEnvironment
+        dataProvider={
+          new StaticTreeDataProvider(longTree.items, (item, data) => ({ ...item, data }))
+        }
+        getItemTitle={(item) => item.data}
+        viewState={{}}
+      >
+        <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+      </UncontrolledTreeEnvironment>
+    </div>
+  );
 };
 
 interface ExplorerProps {}
