2043ff99d24e467611153552bb9b036692f64f25
luoluoqixi <luoqixi@luoqixi.com>
2025-03-26 02:00:41 +0800

    🚧 ctrl s

--- ui/src/renderer/src/components/editor/markdown/MarkdownEditor.tsx
+++ ui/src/renderer/src/components/editor/markdown/MarkdownEditor.tsx
@@ -1,6 +1,9 @@
+import { EditorView } from '@codemirror/view';
+import { commandsCtx, editorViewOptionsCtx } from '@milkdown/core';
 import { Crepe } from '@milkdown/crepe';
 import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/frame.css';
+import { $command, $useKeymap } from '@milkdown/utils';
 import {
   CSSProperties,
   Ref,
@@ -47,18 +50,73 @@ export default forwardRef((props: CodeMirrorEditorProps, ref: Ref<MarkdownEditor
     const crepe = new Crepe({
       root: editorRef.current,
       defaultValue,
+      features: {
+        placeholder: false,
+      },
+      featureConfigs: {
+        [Crepe.Feature.ImageBlock]: {
+          proxyDomURL: (originalURL: string) => {
+            console.log(originalURL);
+            return originalURL;
+          },
+        },
+        [Crepe.Feature.BlockEdit]: {
+          handleAddIcon: undefined,
+        },
+      },
     });
-    crepe.setReadonly(readOnly || false);
-    crepe.create().then(() => {
-      // 上一个编辑器销毁时可能还会还会短暂占用dom导致鼠标在div上move时有一些事件报错, 延迟一点点时间解决
-      setTimeout(() => setLoading(false), 50);
+
+    const saveCommand = $command('saveCommand', () => () => {
+      return () => {
+        const mdText = crepe.getMarkdown();
+        // console.log(mdText);
+        onSave?.(mdText);
+        return true;
+      };
     });
+    const saveKeyMap = $useKeymap('saveKeymap', {
+      saveDescription: {
+        shortcuts: 'Ctrl-s',
+        command: (ctx) => {
+          const commands = ctx.get(commandsCtx);
+          return () => commands.call(saveCommand.key);
+        },
+      },
+    });
+    crepe.editor.use([saveCommand, saveKeyMap].flat());
+
+    crepe.on((listener) => {
+      // listener.markdownUpdated((ctx, md, prevMd) => {
+      //   console.log(ctx, md, prevMd);
+      // });
+      listener.updated((ctx, doc, prevDoc) => {
+        console.log(ctx, doc, prevDoc);
+        // const editorView = crepe.editor.ctx.get(editorViewCtx);
+      });
+    });
+
+    crepe.editor.config((ctx) => {
+      ctx.update(editorViewOptionsCtx, (prev) => ({
+        ...prev,
+        attributes: {
+          ...prev.attributes,
+          spellcheck: 'false',
+        },
+      }));
+    });
+    crepe
+      .setReadonly(readOnly || false)
+      .create()
+      .then(() => {
+        // 上一个编辑器销毁时可能还会还会短暂占用dom导致鼠标在div上move时有一些事件报错, 延迟一点点时间解决
+        setTimeout(() => setLoading(false), 50);
+      });
     updateCrepe(() => () => crepe);
     return () => {
       updateCrepe(null);
       if (crepe) crepe.destroy();
     };
-  }, [fileName]);
+  }, [fileName, onSave, onUpdateListener]);
 
   useEffect(() => {
     if (!getCrepe) return;
