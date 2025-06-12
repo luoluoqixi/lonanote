299f3ecbcb3023e5a85a075ece7c791366edccbf
luoluoqixi <luoqixi@luoqixi.com>
2025-06-13 01:38:48 +0800

    🚧 mobile ui: video view #15

--- ui/flutter/lib/src/views/editor/video_view_page.dart
+++ ui/flutter/lib/src/views/editor/video_view_page.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lonanote/src/common/utility.dart';
 import 'package:lonanote/src/widgets/platform_page.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoViewPage extends ConsumerStatefulWidget {
   const VideoViewPage({
@@ -16,9 +20,31 @@ class VideoViewPage extends ConsumerStatefulWidget {
 }
 
 class _VideoViewPageState extends ConsumerState<VideoViewPage> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+
   @override
   void initState() {
     super.initState();
+    _videoPlayerController = VideoPlayerController.file(File(widget.path))
+      ..initialize().then((_) {
+        setState(() {
+          _chewieController = ChewieController(
+            videoPlayerController: _videoPlayerController,
+            autoPlay: true,
+            looping: false,
+            allowPlaybackSpeedChanging: true,
+            aspectRatio: _videoPlayerController.value.aspectRatio,
+          );
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,7 +54,19 @@ class _VideoViewPageState extends ConsumerState<VideoViewPage> {
     return PlatformSimplePage(
       titleText: name,
       noScrollView: true,
-      child: Text("video"),
+      child: SafeArea(
+        bottom: true,
+        top: false,
+        left: false,
+        right: false,
+        child: _chewieController != null &&
+                _chewieController!.videoPlayerController.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _chewieController!.aspectRatio!,
+                child: Chewie(controller: _chewieController!),
+              )
+            : const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
