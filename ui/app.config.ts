6f9e71f45f77fae22a8bee54a97a606b0a2236dc
luoluoqixi <luoqixi@luoqixi.com>
2026-05-01 03:14:14 +0800

    🚧 dev

--- ui/app.config.ts
+++ ui/app.config.ts
@@ -1,51 +1,64 @@
 import { type ConfigContext, type ExpoConfig } from "expo/config";
 
-import { withDebugSuffix } from "./tools/build_plugins/withDebugSuffix";
+const IS_DEV = process.env.APP_VARIANT === "development";
 
-export default ({ config }: ConfigContext): ExpoConfig =>
-  withDebugSuffix({
-    ...config,
-    name: "lonanote",
-    slug: "lonanote",
-    version: "1.1.0",
-    orientation: "default",
-    icon: "./assets/images/icon.png",
-    scheme: "lonanote",
-    userInterfaceStyle: "automatic",
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.luoluoqixi.lonanote",
-      // icon: "./assets/expo.icon",
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.luoluoqixi.lonanote.dev";
+  }
+  return "com.luoluoqixi.lonanote";
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "lonanote-dev";
+  }
+  return "lonanote";
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: getAppName(),
+  slug: "lonanote",
+  version: "1.1.0",
+  orientation: "default",
+  icon: "./assets/images/icon.png",
+  scheme: "lonanote",
+  userInterfaceStyle: "automatic",
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: getUniqueIdentifier(),
+    // icon: "./assets/expo.icon",
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
     },
-    android: {
-      adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
-        monochromeImage: "./assets/images/android-icon-monochrome.png",
-      },
-      predictiveBackGestureEnabled: false,
-      package: "com.luoluoqixi.lonanote",
-    },
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          backgroundColor: "#208AEF",
-          android: {
-            image: "./assets/images/splash-icon.png",
-            imageWidth: 76,
-          },
+    predictiveBackGestureEnabled: false,
+    package: getUniqueIdentifier(),
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        backgroundColor: "#208AEF",
+        android: {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 76,
         },
-      ],
+      },
     ],
-    experiments: {
-      typedRoutes: true,
-      reactCompiler: true,
-    },
-  });
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+});
