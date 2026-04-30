d4c2d696c1732a4e838c5bdea47f09844c53593b
luoluoqixi <luoqixi@luoqixi.com>
2026-05-01 02:15:50 +0800

    🚧 dev app name

--- ui/tools/build_plugins/withDebugSuffix.js
+++ ui/tools/build_plugins/withDebugSuffix.js
@@ -4,6 +4,7 @@ const { withAppBuildGradle } = require("@expo/config-plugins");
  * 插件：自动为 Android debug 构建加 .dev 后缀
  */
 const DEBUG_SUFFIX = ".dev";
+const DEBUG_NAME_SUFFIX = "-DEBUG";
 
 const withDebugSuffix = (config) => {
   return withAppBuildGradle(config, (configObj) => {
@@ -14,7 +15,7 @@ const withDebugSuffix = (config) => {
       /(\n\s{4}buildTypes\s*\{\s*\n\s{8}debug\s*\{)([\s\S]*?)(\n\s{8}\}\s*\n\s{8}release\s*\{)/m,
       (match, p1, p2, p3) => {
         if (/applicationIdSuffix/.test(p2)) return match;
-        return `${p1}${p2}\n            applicationIdSuffix "${DEBUG_SUFFIX}"${p3}`;
+        return `${p1}${p2}\n            applicationIdSuffix "${DEBUG_SUFFIX}"\n            versionNameSuffix "${DEBUG_NAME_SUFFIX}"${p3}`;
       },
     );
     configObj.modResults.contents = gradle;
