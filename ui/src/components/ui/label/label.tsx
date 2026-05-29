6fce66d300ab4b6459e4e461c28d7346933fe35a
luoluoqixi <luoqixi@luoqixi.com>
2026-05-30 02:23:03 +0800

    🚧 label style

--- ui/src/components/ui/label/label.tsx
+++ ui/src/components/ui/label/label.tsx
@@ -1,7 +1,17 @@
+import type { CSSProperties } from "react";
 import { Label as TamaguiLabel } from "tamagui";
+
+import { isWeb } from "@/api/common/platform";
 
 import type { LabelProps } from "./types";
 
 export function Label(props: LabelProps) {
-  return <TamaguiLabel {...props} />;
+  return (
+    <TamaguiLabel
+      {...props}
+      style={
+        isWeb() ? ([{ userSelect: "text" } as CSSProperties, props.style] as const) : props.style
+      }
+    />
+  );
 }
