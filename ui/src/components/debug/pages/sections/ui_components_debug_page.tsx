bb434626a53af119926253275222d5087b83466c
luoluoqixi <luoqixi@luoqixi.com>
2026-06-24 22:18:44 +0800

    🚧 dismiss on snap to bottom

--- ui/src/components/debug/pages/sections/ui_components_debug_page.tsx
+++ ui/src/components/debug/pages/sections/ui_components_debug_page.tsx
@@ -1550,6 +1550,7 @@ export function UiComponentsDebugPage() {
               </Button>
             </View>
           }
+          dismissOnSnapToBottom
           handle
           modal
           native={false}
