d27e9e6dd772365e74f259319edb02674092befa
luoluoqixi <luoqixi@luoqixi.com>
2026-07-05 22:50:43 +0800

    🚧 ios theme

--- ui/src/app/(home)/_layout.tsx
+++ ui/src/app/(home)/_layout.tsx
@@ -81,7 +81,6 @@ export default function UILayout() {
                 color: theme.color.val,
               },
               headerTransparent: true,
-              ...transparentHeaderFallback,
               title: getAppHomeTitle(),
             });
           }
